95d83eb7ae9c3298580f92cfc978abe5510e3ff9
Yadu-M <ymadhu@torontomu.ca>
2024-08-26 16:11:33 -0400

    added key resource

--- flaskr/api/__init__.py
+++ flaskr/api/__init__.py
@@ -1,12 +1,33 @@
-import os
-
 from flask import Blueprint
 from flask_restful import Api
 
-from flaskr.dao.redis.key_schema import KeySchema
+from redis import Redis
+from redis import exceptions
 
-blueprint = Blueprint("api", __name__)
+from flaskr.api.keys import ApiKeyResource
+
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+blueprint = Blueprint('api', __name__, url_prefix='/api')
 api = Api(blueprint)
 
-def configue(app):
-  key_schema = KeySchema(app.config['REDIS_KEY_PREFIX'])
+def configure(app):
+    # Initialize Redis client
+  REDIS_PASSWORD = os.getenv('REDIS_PASSWORD')  
+  
+  redis_client = Redis(
+      host=app.config['REDIS_HOST'],
+      port=app.config['REDIS_PORT'],
+      password=REDIS_PASSWORD
+  )
+  
+  # Check Redis connection
+  try:
+      redis_client.ping()
+  except exceptions.AuthenticationError:
+      app.logger.error("Redis authentication failed")
+      
+
+  api.add_resource(ApiKeyResource, '/key')
