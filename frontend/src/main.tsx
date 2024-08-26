d512cbf244d27d7293669e196b67ed543b7d7519
Yadu-M <ymadhu@torontomu.ca>
2024-08-25 21:40:16 -0400

    added frontend

---
+++ frontend/src/main.tsx
@@ -0,0 +1,12 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App.tsx'
+
+const rootElemet = document.getElementById('root')
+
+if (rootElemet)
+createRoot(rootElemet).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
