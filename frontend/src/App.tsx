ee2278905cae08a4ee62240d0abd31dcc7c3ec9f
Yadu-M <ymadhu@torontomu.ca>
2024-08-26 16:09:08 -0400

    added example map

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,18 @@
+import './App.css'
+import { MapC } from './Components/MapC'
+import Layout from './Layout'
+
 function App() {
   return (
     <>
-      <h1>Hello</h1>
+      <Layout>
+        <main>
+          <h1 style={{
+            paddingLeft: "1rem"
+          }}>Realtime mapping of Festivals across TO</h1>
+          <MapC />
+        </main>
+      </Layout>
     </>
   )
 }
