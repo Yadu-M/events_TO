c0d2d1549d6d1bedcad0f144ec9d2c3eab9e4307
Yadu-M <ymadhu@torontomu.ca>
2024-11-09 21:14:41 -0500

    added basic hover hook

---
+++ frontend/src/Hooks/useHover.tsx
@@ -0,0 +1,22 @@
+import { RefObject, useEffect, useState } from "react";
+
+export const useHover = <T extends HTMLElement>(ref: RefObject<T>) => {
+  const [isHovered, setIsHovered] = useState(false);
+
+  useEffect(() => {
+    const node = ref.current;
+    if (!node) return;
+
+    console.log('here')
+
+    const handeMouseEnter = () => setIsHovered(true);
+    const handleMouseLeave = () => setIsHovered(false);
+
+    return () => {
+      node.removeEventListener("mouseenter", handeMouseEnter);
+      node.removeEventListener("mouseleave", handleMouseLeave);
+    };
+  }, [ref]);
+
+  return isHovered;
+};
