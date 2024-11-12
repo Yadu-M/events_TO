b758792fa0723b5ee7a783184a9d0b9ba69fa03a
Yadu-M <ymadhu@torontomu.ca>
2024-11-12 18:31:37 -0500

    backend: added call to fetch a specific event

--- backend/api/event.py
+++ backend/api/event.py
@@ -18,6 +18,16 @@ def get_events():
     abort(404, description="Resource not found")
   return jsonify({ "status": "success", "data": results })
 
+@bp.route('/<event_id>')
+def get_specific_event(event_id):
+  db = get_db()
+  result = {}
+  db.row_factory = dict_factory
+  try:
+    results = db.execute("SELECT * FROM event WHERE event.id = ?", (event_id, )).fetchone()
+  except Exception:
+    abort(404, description="Resource not found")
+  return jsonify({ "status": "success", "data": results })
 
 @bp.route('/getAll')
 def get_all_events():
