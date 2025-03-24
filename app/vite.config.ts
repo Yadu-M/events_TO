02ca8b5bb54dc8183c091a250c5893d24f081ce1
Yadu-M <ymadhu@torontomu.ca>
2025-03-24 09:52:10 -0400

    update api url

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -5,7 +5,7 @@ import path from 'path';
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
 	// Load env variables based on the mode (development, production, etc.)
-	const env = loadEnv(mode, process.cwd(), '');
+	// const env = loadEnv(mode, process.cwd(), '');
 	return {
 		plugins: [react()],
 		server: {
